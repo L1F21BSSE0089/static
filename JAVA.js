8a44d62cf194774dbb3a191eaa86b936b4130515
Rashid Ali <l1f21bsse0089@ucp.edu.pk>
2025-01-21 17:22:42 +0500

    Create JAVA.js

---
+++ JAVA.js
@@ -0,0 +1,4 @@
+// JavaScript to handle the button click
+document.getElementById('clickMeBtn').addEventListener('click', function() {
+  document.getElementById('responseText').textContent = "Hello, you've clicked the button!";
+});
